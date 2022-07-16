cfd6e32f8c791425ca85f40153d9377bc1936cfe
Woohyun <ktkwhms3@gmail.com>
2022-07-16 17:30:57 +0900

    Feat: 이미지 나열,
    Fix: CameraRoll lib 오류

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,43 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import {Alert,View,Text} from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  FlatList,
+  PermissionsAndroid,
+  Platform,
+} from 'react-native';
 
 import Styled from 'styled-components/native';
-
-import * as ImagePicker from "react-native-image-picker"
 import CameraRoll from "@react-native-community/cameraroll";
 
-const App = () => {
-  const [imageSource, setImageSource] = useState();
 
-  useEffect(()=>{
+const App = () => {
+  const [isState, setIsState] = useState();
+  useEffect(() => {
     CameraRoll.getPhotos({
     first: 20,
            assetType: 'Photos',
          })
          .then(r => {
-           setImageSource({ photos: r.edges });
+            console.log(r)
+           setIsState({ photos: r.edges });
          })
          .catch((err) => {
             //Error Loading Images
          });
-  },[])
+    }, []);
+    const arr = [
+    "https://item.kakaocdn.net/do/493188dee481260d5c89790036be0e66effd194bae87d73dd00522794070855d",
+    "https://item.kakaocdn.net/do/493188dee481260d5c89790036be0e66effd194bae87d73dd00522794070855d",
+    "https://item.kakaocdn.net/do/493188dee481260d5c89790036be0e66effd194bae87d73dd00522794070855d",
+    "https://item.kakaocdn.net/do/493188dee481260d5c89790036be0e66effd194bae87d73dd00522794070855d",
+    "https://item.kakaocdn.net/do/493188dee481260d5c89790036be0e66effd194bae87d73dd00522794070855d",
+    ]
 
    return (
      <View>
-       <ScrollView>
-         {this.state.photos.map((p, i) => {
-         return (
-           <Image
-             key={i}
-             style={{
-               width: 300,
-               height: 100,
-             }}
-             source={{ uri: p.node.image.uri }}
-           />
-         );
-       })}
-       </ScrollView>
+     <Text>시발</Text>
+     <FlatList data={arr} renderItem = {({item})=> <Image style={{width:100, height:100}} source={{uri:item}}/>}/>
      </View>
    );
 };
