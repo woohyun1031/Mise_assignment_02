251dadeed6a1112952e0f21f77b1923b56689dfc
Woohyun <ktkwhms3@gmail.com>
2022-07-16 17:51:16 +0900

    Feat: 3장씩 나열하여 디바이스 이미지 가져오기
    Feat: 3장씩 나열하여 디바이스 이미지 가져오기 성공!!

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -6,26 +6,46 @@ import {
   FlatList,
   PermissionsAndroid,
   Platform,
+  ScrollView,
 } from 'react-native';
 
 import Styled from 'styled-components/native';
 import CameraRoll from "@react-native-community/cameraroll";
 
 
+
 const App = () => {
-  const [isState, setIsState] = useState();
+  const [isState, setIsState] = useState({data:''});
+  const checkPlatform = async() => {
+    if (Platform.OS === 'android') {
+                const result = await PermissionsAndroid.request(
+                  PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+                  {
+                    title: 'Permission Explanation',
+                    message: 'ReactNativeForYou would like to access your photos!',
+                  },
+                );
+                if (result !== 'granted') {
+                  console.log('Access to pictures was denied');
+                  return;
+                }
+              }
+              CameraRoll.getPhotos({
+                  first: 20,
+                         assetType: 'Photos',
+                       })
+                       .then(r => {
+                          console.log(r)
+                         setIsState({ data: r.edges });
+                       })
+                       .catch((err) => {
+                          //Error Loading Images
+                          console.log(err)
+                       });
+  }
+
   useEffect(() => {
-    CameraRoll.getPhotos({
-    first: 20,
-           assetType: 'Photos',
-         })
-         .then(r => {
-            console.log(r)
-           setIsState({ photos: r.edges });
-         })
-         .catch((err) => {
-            //Error Loading Images
-         });
+    checkPlatform();
     }, []);
     const arr = [
     "https://item.kakaocdn.net/do/493188dee481260d5c89790036be0e66effd194bae87d73dd00522794070855d",
@@ -37,8 +57,7 @@ const App = () => {
 
    return (
      <View>
-     <Text>시발</Text>
-     <FlatList data={arr} renderItem = {({item})=> <Image style={{width:100, height:100}} source={{uri:item}}/>}/>
+     <FlatList data={isState.data} numColumns={3} renderItem = {({item})=> <Image style={{width:'33%', height:150}} source={{ uri: item.node.image.uri }}/>}/>
      </View>
    );
 };
